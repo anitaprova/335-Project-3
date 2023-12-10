12c2fd3da6dfb0eaf00e0b33921e2b148a55329e
anitaprova <104943181+anitaprova@users.noreply.github.com>
2023-12-09 21:06:37 -0500

    testing for different countries

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@
 int main()
 {
 	// Nearest Neighbor
-	nearestNeighbor("input_files/dj38.tsp");
-	nearestNeighbor("input_files/wi29.tsp");
+	// nearestNeighbor("input_files/dj38.tsp");
+	// nearestNeighbor("input_files/zi929.tsp");
+	// nearestNeighbor("input_files/mu1979.tsp");
+	// nearestNeighbor("input_files/fi10639.tsp");
 
 	return 0;
 }
